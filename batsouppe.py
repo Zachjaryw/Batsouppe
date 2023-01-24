19d2092b88ee6a261cba561c34ba57e7506e1665
Zachary Werner <85905928+Zachjaryw@users.noreply.github.com>
2023-01-23 17:19:45 -0800

    Create batsouppe.py

---
+++ batsouppe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy
+import json
+from Dropbox_Setup import initializeToken as IT
+from Dropbox_Setup import fromDBX
+from Dropbox_Setup import toDBX
+import streamlit as st
+
+st.title('Batsouppe Inventory')
